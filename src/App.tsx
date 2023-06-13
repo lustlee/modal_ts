f2ec97609249e6d8b833fd306e84f5479bf4f0e2
Ramazan <r.tokoshev@gmail.com>
2023-06-13 23:55:56 +0600

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
-
+import React, {useState} from 'react';
+import Modal from "./componetns/Modal/Modal";
+import ACE from './assets/ace.jpg'
 const App = () => {
-  return (
+    const [open, setOpen] = useState(false);
+
+    return (
       <div className="App">
-        <button className="open-modal-btn">⚛️ Open modal</button>
-        {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+        <button
+            onClick={() => setOpen(true)}
+            className="open-modal-btn">⚛️ Open modal</button>
+          <Modal open={open} setOpen={setOpen}>
+              <img src={ACE} alt={ACE}/>
+              <h3>One Piece!</h3>
+              <p>This is my Modal</p>
+          </Modal>
       </div>
   );
 };
